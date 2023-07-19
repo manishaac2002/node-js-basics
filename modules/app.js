c0dca96cb70c40632aca6387096432dec4862ecd
manishaac2002 <manishaac2002@gmail.com>
2023-07-19 10:33:14 +0530

    folder structure.

--- modules/app.js
+++ modules/app.js
@@ -1,8 +1,10 @@
 //create http server
 const http = require('http')
 //importing requestHandler
-const router = require('./router')
+const router = require('./router.js')
 
 console.log(router.sample);
 const server =http.createServer(router.handler)
 server.listen(3000)
+
+// while running this file ./foldername./filename
