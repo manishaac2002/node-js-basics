1fcf97a466cd76c75f4009267fcf3227b61c8e32
manishaac2002 <manishaac2002@gmail.com>
2023-11-16 11:41:19 +0530

    chore--

--- modules/router.js
+++ modules/router.js
@@ -3,9 +3,12 @@ const fs=require ('fs')
 const requestHandler =(request,response)=>{
     const url =request.url
     console.log(url);// which return /(slash)
+
     const method=request.method
     console.log(method);//which return the type of method like (get,post)
+
    // console.log(request.headers);//request
+
    if (url ==='/') { 
        response.setHeader('Content-type','text/html')//response
        response.write('<html>')
@@ -14,6 +17,7 @@ const requestHandler =(request,response)=>{
        response.write('</html>')
        return response.end()
     }
+
     if (url==='/message' && method =='POST') {
         fs.writeFileSync('hello.txt','dummy')
         response.setHeader('Location','/')
@@ -23,16 +27,16 @@ const requestHandler =(request,response)=>{
         console.log("THIS IS MESSAGE");
         return response.end()
     }
+
     response.setHeader('Content-type','text/html')//response
     response.write('<html>')
     response.write('<head><title>Sample code</title></head>')
     response.write('<body><h1>Hello from Node.js Server..!</h1></body>')
     response.write('</html>')
     response.end()
-
 }
-// module.exports = {handler:requestHandler,
-//                   sample:"sample statement" }
+
+// module.exports = {handler:requestHandler, sample:"sample statement" }
 
 //node js
 // exports.handler =requestHandler
